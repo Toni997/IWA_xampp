6db8eabbc8159d9bfa9785fa491a907664f117cb
Toni Kazinoti <tkazinoti@hrcloud.com>
2022-03-07 14:33:58 +0100

    .htaccess file added

---
+++ 404.py
@@ -0,0 +1,8 @@
+#!C:\Users\tkazinoti\AppData\Local\Programs\Python\Python310\python.exe
+print("Content-Type: text/html\r\n\r\n")
+
+print("""
+
+<img src="./404.png" alt="error 404: page not found">
+
+""")
---
+++ test.py
@@ -0,0 +1,4 @@
+#!C:\Users\tkazinoti\AppData\Local\Programs\Python\Python310\python.exe
+print("Content-Type: text/html\r\n\r\n")
+
+print("<h1>testing</h1>")
